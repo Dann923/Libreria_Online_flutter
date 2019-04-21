1f0de12b8877650bc1799201302709e7c8b631cc
Dann923 <rojasc.dan@gmail.com>
2019-04-20 23:37:20 -0400

    Primer ejemplo funcional

--- lib/widgets/libros/card_libros.dart
+++ lib/widgets/libros/card_libros.dart
@@ -9,28 +9,12 @@ class CardLibros extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // final cardTitle = Text(
-    //   titleCard,
-    //   textDirection: TextDirection.ltr,
-    //   style: TextStyle(
-    //       color: Colors.white,
-    //       fontSize: 24.0,
-    //       fontWeight: FontWeight.w500
-    //   ),
-    // );
-
     final cardArrowFab = Positioned(
       bottom: paddingSize,
       right: paddingSize,
       child: FloatingActionButton(
         //heroTag: "fab1_$titleCard",
         backgroundColor: Colors.black38,
-        // onPressed: (){
-        //     Navigator.push(
-        //     context,
-        //       MaterialPageRoute(builder: (context) => RoomDetail(titleCard, "In this room there are a lot of things that help to you to cook some amazing things!!!", pathImage)),
-        //     );
-        // },
         child: Icon(
           Icons.arrow_forward_ios,
           color: Colors.white,
@@ -38,52 +22,47 @@ class CardLibros extends StatelessWidget {
       ),
     );
 
-    // final cardLightFab = Positioned(
-    //   top: paddingSize,
-    //   right: paddingSize,
-    //   child: FloatingActionButton(
-    //     heroTag: "fab2_$titleCard",
-    //     backgroundColor: Colors.transparent,
-    //     child: Icon(
-    //       Icons.lightbulb_outline,
-    //       color: Colors.white,
-    //     ),
-    //     onPressed: null,
-    //   ),
-    // );
-
     final cardContainer = Container(
-        height: 200.0,
+        height: 230.0,
+        width: 150.0,
         decoration: BoxDecoration(
             image: DecorationImage(
-                image: AssetImage(pathImage),
-                fit: BoxFit.cover
+              image: AssetImage(pathImage),
+              //fit: BoxFit.cover poner la imagen en todo el card
             ),
-            borderRadius: BorderRadius.circular(20.0),
+            //borderRadius: BorderRadius.circular(20.0),
             boxShadow: <BoxShadow>[
               BoxShadow(
-                  color: Colors.black54,
+                  //sombras del card
+                  color: Colors.black,
                   blurRadius: 15.0,
-                  offset: Offset(0.0, 0.75)
-              )
-            ]
-        )
-    );
+                  offset: Offset(0.0, 0.75))
+            ]),
+        /*onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => RoomDetail(
+                    titleCard,
+                    "In this room there are a lot of things that help to you to cook some amazing things!!!",
+                    pathImage)),
+          );
+        }*/
+        );
 
     return Container(
-      margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 30.0),
-      child: Stack(
-        children: <Widget>[
-          cardContainer,
-          Positioned(
-            bottom: paddingSize*2,
-            left: paddingSize*2,
+        margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 30.0),
+        child: Stack(
+          children: <Widget>[
+            cardContainer,
+            //Positioned(
+            //bottom: paddingSize*2,
+            //left: paddingSize*2,
             //child: cardTitle,
-          ),
-          cardArrowFab,
-          //cardLightFab
-        ],
-      )
-    );
+            //),
+            //cardArrowFab,
+            //cardLightFab
+          ],
+        ));
   }
 }
